4a41b8e5a43d2fd64b9ddded652e4fb4f2e7b95c
tiendat8011 <61610622+tiendat8011@users.noreply.github.com>
2021-11-20 21:10:10 +0700

    Update tinhMDDS.js

--- tinhMDDS.js
+++ tinhMDDS.js
@@ -1,11 +1,13 @@
 const matdodanso = ({so_dan, dien_tich }) => {
-    if(!(so_dan>=0 && so_dan <= 7000000000)) return 'INPUT KHONG HOP LE'
-    if(dien_tich <=0 || dien_tich > 5999999) return  "INPUT KHONG HOP LE";
+    let result = '';
+    if(!(so_dan>=0 && so_dan <= 7000000000)) result = 'INPUT KHONG HOP LE'
+    if(dien_tich <=0 || dien_tich > 5999999) result =  "INPUT KHONG HOP LE";
     const matdo = so_dan/dien_tich;
-    if(matdo > 2000000) return "MAT DO DAN SO CUC CAO"
-    else if(matdo >1000000 ) return  "MAT DO DAN SO CAO"
-    else if(matdo > 1000) return "MAT DO DAN SO TRUNG BINH"
-    else if(matdo > 0) return  "MAT DO DAN SO THAP"
-    else return 'MAT DO DAN SO KHONG HOP LE'
+    if(matdo > 2000000) result = "MAT DO DAN SO CUC CAO"
+    else if(matdo >1000000 ) result = "MAT DO DAN SO CAO"
+    else if(matdo > 1000) result = "MAT DO DAN SO TRUNG BINH"
+    else if(matdo > 0) result =  "MAT DO DAN SO THAP"
+    else result = 'MAT DO DAN SO KHONG HOP LE'
+    return result 
 }
 
